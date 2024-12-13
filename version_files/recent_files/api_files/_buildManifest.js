e2a43b3cb97552d0454e91e1b916bdd1b49e1b0f
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-12-13 12:56:23 +0330

    API updated to version: 0a09e465bb8c8f35c779bfe132793db3

---
+++ version_files/recent_files/api_files/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+  __rewrites: {
+    afterFiles: [],
+    beforeFiles: [],
+    fallback: []
+  },
+  "/": ["static/chunks/832-78cf7dd7bae1ce96.js", "static/css/445342179c0db4f3.css", "static/chunks/pages/index-f8c14b6b070bb7a7.js"],
+  "/404": ["static/chunks/pages/404-fd1982be800c4d0c.js"],
+  "/_error": ["static/chunks/pages/_error-53c7c74a0fadb444.js"],
+  sortedPages: ["/", "/404", "/_app", "/_error"]
+}, self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
