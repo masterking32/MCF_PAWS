690577532803f7fcc9693793d8fdd5ae997f362f
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2025-02-04 23:50:06 +0330

    API updated to version: b728b92b3d5a5cd6f64934521c718d94

--- version_files/recent_files/api_files/_buildManifest.js
+++ version_files/recent_files/api_files/_buildManifest.js
@@ -5,7 +5,7 @@ self.__BUILD_MANIFEST = function(s, c) {
       beforeFiles: [],
       fallback: []
     },
-    "/": [s, "static/chunks/797-581af5e09d812c61.js", c, "static/css/ed6ea74e52b2ed7d.css", "static/chunks/pages/index-0122b71a53fc59e3.js"],
+    "/": [s, "static/chunks/797-581af5e09d812c61.js", c, "static/css/ed6ea74e52b2ed7d.css", "static/chunks/pages/index-0fce2863db60e9cd.js"],
     "/404": ["static/chunks/pages/404-3906dc2650dbee03.js"],
     "/_error": ["static/chunks/pages/_error-5842153508ff243d.js"],
     "/app": ["static/chunks/8015bd09-2cee2227272f3c6c.js", s, "static/chunks/918-58d5479ffee08c9d.js", c, "static/css/bb5fccbe81aae4f5.css", "static/chunks/pages/app-94b5ebe4d045f1a7.js"],
