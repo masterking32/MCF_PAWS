489addc3aa5bc26bfbd6b76d6a917c600981bcaa
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2025-02-25 00:15:08 +0330

    API updated to version: f055f31edc02ac7dad6169d4cabe587f

--- version_files/recent_files/api_files/_buildManifest.js
+++ version_files/recent_files/api_files/_buildManifest.js
@@ -8,7 +8,7 @@ self.__BUILD_MANIFEST = function(s, c, e, a) {
     "/": [s, c, "static/chunks/672-b2320d2532892d9b.js", e, "static/css/ed6ea74e52b2ed7d.css", "static/chunks/pages/index-2478457c4c427a6a.js"],
     "/404": ["static/chunks/pages/404-3906dc2650dbee03.js"],
     "/_error": ["static/chunks/pages/_error-5842153508ff243d.js"],
-    "/app": [s, c, "static/chunks/230-bac009514bb0f183.js", e, "static/css/0f9143eb3c01199f.css", "static/chunks/pages/app-3024277f8eeea543.js"],
+    "/app": [s, c, "static/chunks/230-bac009514bb0f183.js", e, "static/css/0f9143eb3c01199f.css", "static/chunks/pages/app-770fdd195244ea57.js"],
     "/discord-verify": [s, a, "static/chunks/pages/discord-verify-f88257febdde432c.js"],
     "/phantom-connect": [a, "static/chunks/pages/phantom-connect-0f84872630a9a76a.js"],
     sortedPages: ["/", "/404", "/_app", "/_error", "/app", "/discord-verify", "/phantom-connect"]
